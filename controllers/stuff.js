55c349c6d90d2e441018f5243b5819cdf694eb34
ShaminK <kurreembokus.s@gmail.com>
2022-02-03 15:45:18 +0100

    fin du projet Openclassrooms

---
+++ controllers/stuff.js
@@ -0,0 +1,75 @@
+const Thing = require('../models/Thing');
+const fs = require('fs');
+
+
+// Création de l'annonce d'objet à vendre
+exports.createThing = (req, res, next) => {
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
+    const thing = new Thing({
+        ...thingObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Modification d'une annonce d'objet à vendre
+exports.modifyThing = (req, res, next) => {
+    const thingObject = req.file ?
+        {
+            ...JSON.parse(req.body.thing),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+
+// Suppression d'une annonce d'objet à vendre
+exports.deleteThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => {
+            const filename = thing.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Thing.findOne({ _id: req.params.id }).then(
+                    (thing) => {
+                        if (!thing) {
+                            return res.status(404).json({
+                                error: new Error('Objet non trouvé !')
+                            })
+                        }
+
+                        if (thing.userId !== req.auth.userId) {
+                            return res.status(400).json({
+                                error: new Error('Requête non autorisée')
+                            })
+                        }
+                        Thing.deleteOne({ _id: req.params.id })
+                            .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                            .catch(error => res.status(400).json({ error }));
+                    }
+                )
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
+
+
+
+};
+
+// Affichage de la page d'un objet à vendre
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+};
+
+// Affichage de la liste des objet à vendre
+exports.getAllThings = (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+};
